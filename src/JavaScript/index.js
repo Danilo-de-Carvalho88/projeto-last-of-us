d540bbe2b4bca070c476560c14d69d00a0cf490f
Danilo-de-Carvalho <danilodecarvalho88@gmail.com>
2025-03-09 00:00:51 -0300

    Criação do JS

---
+++ src/JavaScript/index.js
@@ -0,0 +1,9 @@
+// Passo 1 => Pegar os elementos HTML dos botões:
+const botoesCarrossel = document.querySelectorAll('.botao');
+
+// Passo 2 => Identificar clique do usuário no botão:
+botoesCarrossel.forEach((botao) => {
+    botao.addEventListener('click', () => {
+        
+    });
+});
